b5bf53b4460cc10b775c016eba223686a89011b8
Dmitry <libafer99@gmail.com>
2020-11-16 02:12:32 +0500

    upload for gihub pages

---
+++ docs/precache-manifest.6a3d812e4cd265c8873393656c07c1ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4e3bc24e62277625f7da719f679b32",
+    "url": "/test_app/index.html"
+  },
+  {
+    "revision": "690e7b9a93a73fbdf208",
+    "url": "/test_app/static/css/2.12da0d42.chunk.css"
+  },
+  {
+    "revision": "734908c8cf68d3628d7c",
+    "url": "/test_app/static/css/main.7c94eb62.chunk.css"
+  },
+  {
+    "revision": "690e7b9a93a73fbdf208",
+    "url": "/test_app/static/js/2.9d151d00.chunk.js"
+  },
+  {
+    "revision": "734908c8cf68d3628d7c",
+    "url": "/test_app/static/js/main.18dd969a.chunk.js"
+  },
+  {
+    "revision": "577740571b468029c3bc",
+    "url": "/test_app/static/js/runtime~main.5b3a4ee9.js"
+  }
+]);
